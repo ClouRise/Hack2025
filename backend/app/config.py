0a511feac7069844a061d805bf95da89141a5814
Boris <mr.clourise@gmail.com>
2025-10-25 16:30:34 +0300

    chat_backend

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "7a73351fa847dbc2318eb6774e4f81843c32268b6bae594fb12707f5f9de3bb8"
 
 if not SECRET_KEY:
     raise ValueError("JWT_SECRET_KEY not find")
